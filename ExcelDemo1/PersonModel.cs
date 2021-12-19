c55e250a7ff43c8aa7f80b54cb4532ec541c9a21
Lumiere Valentine <lumiere.valentine@outlook.com>
2021-12-19 18:07:33 -0500

    Add project files.

---
+++ ExcelDemo1/PersonModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ExcelDemo1
+{
+    public class PersonModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
